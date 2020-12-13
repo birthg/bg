5068700967ae591301c39d750ee5272715827c7e
birthg <75948076+birthg@users.noreply.github.com>
2020-12-13 23:47:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+<!--hide
+var password;
+var pass="nandu";
+password=prompt('tell me ur fav nickname i call u','');
+if(password==pass)
+alert('hi gunda');
+else
+ {
+  window.location="birth.html";
+  }
+  //-->
